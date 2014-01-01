3848774caf95c5fb4f7db880f90ef8db006b9f1b
Vance Lucas <vance@vancelucas.com>
2014-01-01 12:38:21 -0600

    Initial layout and setup on middleman

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'bourbon',                      '~> 3.1.8'            # Sass mixins gem
+gem 'jquery-middleman',             '~> 3.0.4'            # jQuery yo
+gem 'middleman',                    '~> 3.2.1'
+gem 'middleman-blog',               '~> 3.3.0'
+gem 'middleman-gh-pages',           '~> 0.0.2'
+gem 'middleman-livereload',         '~> 3.1.0'
+gem 'middleman-syntax',             '~> 1.2.1'
+gem 'redcarpet',                    '~> 3.0.0'
+gem 'slim',                         '~> 2.0.1'
+gem 'thin',                         '~> 1.6.1'
+gem 'turbolinks',                   require: false
+
+group :development do
+  gem "watchr"
+  gem "rb-fsevent"
+  gem "growl_notify"
+end
+
---
+++ config.rb
@@ -0,0 +1,64 @@
+require 'redcarpet'
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+activate :syntax, :line_numbers => true
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+Time.zone = "UTC"
+
+activate :blog do |blog|
+  blog.prefix = "blog"
+  blog.permalink = ":year/:month/:day/:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/:num"
+end
+
+activate :directory_indexes
+
+page "/feed.xml", :layout => false
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+configure :build do
+  # activate :minify_css
+  # activate :minify_javascript
+  activate :cache_buster
+  activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
